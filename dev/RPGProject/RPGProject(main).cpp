4bdc8fd51000a46998d874559b331b23e336dd51
Corion Reed <CTReed@student.fullsail.edu>
2026-08-11 13:41:19 -0400

    Changed the selectors so when you input the wrong number it will ask for another input

--- dev/RPGProject/RPGProject(main).cpp
+++ dev/RPGProject/RPGProject(main).cpp
@@ -10,6 +10,7 @@
 int main()
 {
     std::cout << "Hello World!\n";
+	//vectors for weapons, armors, monsters and characters
 	std::vector<Item> weapons =
 	{
 		Item("none", "Weapon", 0, 0, 0),
@@ -30,41 +31,42 @@ int main()
 		Monster("Orc", 80, 8, 12, 4),
 		Monster("Dragon", 150, 15, 20, 10)
 	};
-
-	for (const auto& weapon : weapons)
-	{
-		std::cout << "Weapon: " << weapon.GetName() << ", Type: " << weapon.GetType()
-			<< ", Attack Power: " << weapon.GetMinAtt() << " - " << weapon.GetMaxAtt() << std::endl;
-		
-	}
-	std::cout << std::endl;
-	for (const auto& armor : armors)
-	{
-		std::cout << "Armor: " << armor.GetName() << ", Type: " << armor.GetType()
-			<< ", Defense Power: " << armor.GetDefP() << std::endl;
-	}
-	std::cout << std::endl;
-	for (const auto& monster : monsters)
-	{
-		std::cout << "Monster: " << monster.GetName() << ", HP: " << monster.GetHp()
-			<< ", Attack Power: " << monster.GetMinAttP() << " - " << monster.GetMaxAttP()
-			<< ", Defense Power: " << monster.GetDefP() << std::endl;
-	}
-	
 	std::vector<Character> characters =
 	{
 		Character("Lucy", 100, 15, 10, true, weapons[0], armors[0]),
 		Character("AZ", 120, 12, 15, true, weapons[0], armors[0]),
 		Character("Levy", 80, 18, 8, true, weapons[0], armors[0])
-		
+
 	};
-	for (const auto& character : characters)
-	{
-		std::cout << "Character: " << character.GetName() << ", HP: " << character.GetHp() << ", Attack Power: " << character.GetAttP()
-			<< ", Defense Power: " << character.GetDefP() << ", Unlocked: " << (character.GetUnlocked() ? "Yes" : "No")
-			<< ", Weapon: " << character.GetWeapon().GetName() << ", Armor: " << character.GetArmor().GetName()
-			<< std::endl;
-	}
+
+	//for (const auto& weapon : weapons)
+	//{
+	//	std::cout << "Weapon: " << weapon.GetName() << ", Type: " << weapon.GetType()
+	//		<< ", Attack Power: " << weapon.GetMinAtt() << " - " << weapon.GetMaxAtt() << std::endl;
+	//	
+	//}
+	//std::cout << std::endl;
+	//for (const auto& armor : armors)
+	//{
+	//	std::cout << "Armor: " << armor.GetName() << ", Type: " << armor.GetType()
+	//		<< ", Defense Power: " << armor.GetDefP() << std::endl;
+	//}
+	//std::cout << std::endl;
+	//for (const auto& monster : monsters)
+	//{
+	//	std::cout << "Monster: " << monster.GetName() << ", HP: " << monster.GetHp()
+	//		<< ", Attack Power: " << monster.GetMinAttP() << " - " << monster.GetMaxAttP()
+	//		<< ", Defense Power: " << monster.GetDefP() << std::endl;
+	//}
+	//std::cout << std::endl;
+	//for (const auto& character : characters)
+	//{
+	//	std::cout << "Character: " << character.GetName() << ", HP: " << character.GetHp() << ", Attack Power: " << character.GetAttP()
+	//		<< ", Defense Power: " << character.GetDefP() << ", Unlocked: " << (character.GetUnlocked() ? "Yes" : "No")
+	//		<< ", Weapon: " << character.GetWeapon().GetName() << ", Armor: " << character.GetArmor().GetName()
+	//		<< std::endl;
+	//}
+	//std::cout << std::endl;
 	std::cout << "Choose your character: \n";
 
 	for (size_t i = 0; i < characters.size(); ++i)
@@ -78,22 +80,22 @@ int main()
 	/*this is the character selection loop*/
 	Character selectedCharacter;
 	int choice;
-	std::cout << "Enter the number of your choice: ";
-	std::cin >> choice;
-	
-	if (choice >= 1 && choice <= characters.size() && characters[choice - 1].GetUnlocked())
+	bool validChoice = false;
+	do
 	{
-		selectedCharacter = characters[choice - 1];
-		std::cout << "You selected: " << selectedCharacter.GetName() << std::endl;
-
-
-
-	}
-	
-	else
-	{
-		std::cout << "Invalid choice. Please select a valid character." << std::endl;
-	}
+		std::cout << "Enter the number of your choice: ";
+		std::cin >> choice;
+		if (choice >= 1 && choice <= characters.size() && characters[choice - 1].GetUnlocked())
+		{
+			selectedCharacter = characters[choice - 1];
+			validChoice = true;
+			std::cout << "You selected: " << selectedCharacter.GetName() << std::endl;
+		}
+		else
+		{
+			std::cout << "Invalid choice. Please select a valid character." << std::endl;
+		}
+	} while (!validChoice);
 
 	/*weapon selection loop*/
 	std::cout << "Choose your Weapon: \n";
@@ -102,17 +104,23 @@ int main()
 		std::cout << i + 1 << ". " << weapons[i].GetName()<< ", Attack Power: " << weapons[i].GetMinAtt() << " - " << weapons[i].GetMaxAtt() << std::endl;
 	}
 	int wChoice;
-	std::cout << "Enter the number of your choice: ";
-	std::cin >> wChoice;
-	if (wChoice >= 1 && wChoice <= weapons.size())
+	validChoice = false;
+	do
 	{
-		selectedCharacter.setWeapon(weapons[wChoice - 1]);
-		std::cout << "You equipped: " << selectedCharacter.GetWeapon().GetName() << std::endl;
-	}
-	else
-	{
-		std::cout << "Invalid choice. Please select a valid weapon." << std::endl;
-	}
+		std::cout << "Enter the number of your choice: ";
+		std::cin >> wChoice;
+		if (wChoice >= 1 && wChoice <= weapons.size())
+		{
+			selectedCharacter.setWeapon(weapons[wChoice - 1]);
+			validChoice = true;
+			std::cout << "You equipped: " << selectedCharacter.GetWeapon().GetName() << std::endl;
+		}
+		else
+		{
+			std::cout << "Invalid choice. Please select a valid weapon." << std::endl;
+		}
+	} while (!validChoice);
+
 	/*armor selection loop*/
 	std::cout << "Choose your Armor: \n";
 	for (size_t i = 0; i < armors.size(); ++i)
@@ -120,17 +128,48 @@ int main()
 		std::cout << i + 1 << ". " << armors[i].GetName() << " Defence Stat: " << armors[i].GetDefP() << std::endl;
 	}
 	int aChoice;
-	std::cout << "Enter the number of your choice: ";
-	std::cin >> aChoice;
-	if (aChoice >= 1 && aChoice <= armors.size())
+	validChoice = false;
+	do
 	{
-		selectedCharacter.setArmor(armors[aChoice - 1]);
-		std::cout << "You equipped: " << selectedCharacter.GetArmor().GetName() << std::endl;
-	}
-	else
+		std::cout << "Enter the number of your choice: ";
+		std::cin >> aChoice;
+		if (aChoice >= 1 && aChoice <= armors.size())
+		{
+			selectedCharacter.setArmor(armors[aChoice - 1]);
+			validChoice = true;
+			std::cout << "You equipped: " << selectedCharacter.GetArmor().GetName() << std::endl;
+		}
+		else
+		{
+			std::cout << "Invalid choice. Please select a valid armor." << std::endl;
+		}
+	} while (!validChoice);
+
+	
+
+	/*monster selection loop*/
+	for (size_t i = 0; i < monsters.size(); ++i)
 	{
-		std::cout << "Invalid choice. Please select a valid piece of armor." << std::endl;
+		std::cout << i + 1 << ". " << monsters[i].GetName() << std::endl;
 	}
+	int mChoice;
+	validChoice = false;
+	do 
+	{
+		std::cout << "Choose a monster to fight: ";
+		std::cin >> choice;
+		if (choice >= 1 && choice <= monsters.size())
+		{
+			Monster selectedMonster = monsters[choice - 1];
+			validChoice = true;
+			std::cout << "You selected: " << selectedMonster.GetName() << std::endl;
+		}
+		else
+		{
+			std::cout << "Invalid choice. Please select a valid monster." << std::endl;
+		}
+	} while (!validChoice);
+	
 	
 	return 0;
 }
